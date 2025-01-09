13658a516f23fd40fec8651f62c785fb556f398c
Stefan Sjöberg <bird.nerd@outlook.com>
2025-01-09 15:45:49 +0100

    Update comment in Program.cs to correct Swedish word
    Changed "uppgift" to "övning" in the comment referencing a YouTube video. The updated comment now reads: "Använde den här videon i denna egna övning: https://www.youtube.com/watch?v=ub3P8c87cwk".

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -4,7 +4,7 @@ IPayment payment = PaymentFactory.create(PaymentMethod.ApplePay);
 payment.Pay(1000.0);
 
 /// <summary>
-/// Använde den här videon i denna egna uppgift: https://www.youtube.com/watch?v=ub3P8c87cwk
+/// Använde den här videon i denna egna övning: https://www.youtube.com/watch?v=ub3P8c87cwk
 /// </summary>
 
 Console.ReadKey();
